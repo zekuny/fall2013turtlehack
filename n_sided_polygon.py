05b9b5366c12171951967fa2a3ca9a5b3415757f
Dave Pcolar <pcolar@email.unc.edu>
2013-10-07 22:44:38 -0400

    add n-sided polygon code

---
+++ n_sided_polygon.py
@@ -0,0 +1,44 @@
+import turtle
+import turtlehack
+import random
+
+# A function that draws an n-sided polygon
+def n_sided_polygon(turtle, n, color="#FFFFFF", line_thickness=1):
+	'''
+	Draw an n-sided polygon
+	input: turtle, n, line_length
+	'''
+	# for n times:
+	# Draw a line, then turn 360/n degrees and draw another
+
+	# set initial parameters
+  	turtle.degrees()
+	line_length=80
+  	turtle.pensize(line_thickness)
+  	turn_angle = (360/n)
+  	i = 1
+
+	# Draw each line segment and turn
+
+  	while (i <= n):
+		turtle.color(color)
+    		turtle.pendown()
+    		turtle.forward(line_length)
+   		turtle.penup()
+    		turtle.right(turn_angle)
+    		i += 1
+
+	return 0
+ 
+## MAIN ##
+# set initial parameters
+n=random.randint(3,12)
+
+# create the Turle instance
+graphic = turtle.Turtle()
+
+turtlehack.n_sided_polygon(graphic, n, turtlehack.random_color(), random.randint(4,8))
+
+ignore = input("hit any key to continue:")
+graphic.done()
+
